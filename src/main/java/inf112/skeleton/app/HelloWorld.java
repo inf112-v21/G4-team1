c799a809127e827ef8120ce9a5f4c8ee4e6a7b98
Anders Refvik Torsvik <andersrt@hotmail.no>
2021-02-08 18:11:10 +0100

    Første versjon av kart Blir ikke displayet enda Inneholder tms og tsx filer

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -4,24 +4,57 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class HelloWorld implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
 
+    private TiledMap map;
+    private TmxMapLoader mapLoader;
+
+    private TiledMapTileLayer baseLayer;
+    private TiledMapTileLayer hole;
+    private TiledMapTileLayer flag;
+    private TiledMapTileLayer player;
+
+    private OrthographicCamera camera;
+    private OrthogonalTiledMapRenderer rederer;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
-        font = new BitmapFont();
-        font.setColor(Color.RED);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("RoboRallyTile.tmx");
+
+        camera = new OrthographicCamera();
+        rederer = new OrthogonalTiledMapRenderer(map,1/300);
+        rederer.setView(camera);
+
+        baseLayer = (TiledMapTileLayer) map.getLayers().get("BaseLayer");
+        hole = (TiledMapTileLayer) map.getLayers().get("Hole");
+        flag = (TiledMapTileLayer) map.getLayers().get("Flag");
+        player = (TiledMapTileLayer) map.getLayers().get("Player");
+
+        camera.setToOrtho(false,11,11);
+        camera.x
+        camera.update();
+
+        rederer.setView(camera);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        font.dispose();
     }
 
     @Override
@@ -29,9 +62,7 @@ public class HelloWorld implements ApplicationListener {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
-        batch.end();
+        rederer.render();
     }
 
     @Override
--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -6,9 +6,11 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-        cfg.setTitle("hello-world");
-        cfg.setWindowedMode(480, 320);
+        cfg.setTitle("RoboTestApp");
+        cfg.setWindowedMode(500, 500);
 
         new Lwjgl3Application(new HelloWorld(), cfg);
+
+
     }
 }
