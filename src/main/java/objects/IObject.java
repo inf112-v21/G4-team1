20885ee2eb8e4166f95b3de55602ccf10ae68555
Kristian Oterholt <Kristian.Oterholt@student.uib.no>
2021-02-08 18:52:41 +0100

    lagt metodene i interface

--- src/main/java/objects/IObject.java
+++ src/main/java/objects/IObject.java
@@ -1,4 +1,9 @@
 package objects;
 
 public interface IObject {
+    public void setXPosition(int x);
+    public void setYPosition(int y);
+
+    public int getX();
+    public int getY();
 }
--- src/main/java/objects/flag.java
+++ src/main/java/objects/flag.java
@@ -1,4 +1,23 @@
 package objects;
 
 public class flag implements IObject{
+    @Override
+    public void setXPosition(int x) {
+
+    }
+
+    @Override
+    public void setYPosition(int y) {
+
+    }
+
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
 }
