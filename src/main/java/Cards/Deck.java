12b730e39df3711bb3d22fbd2dd1a353c3741447
AsgeirAlbretsen <bjellanda@gmail.com>
2021-02-24 17:41:39 +0100

    Lagde metoder for å legge kort til deck

--- src/main/java/Cards/Deck.java
+++ src/main/java/Cards/Deck.java
@@ -1,51 +1,79 @@
 package Cards;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
     ArrayList<ICards> cardDeck;
 
     public Deck(){
-        //Move 1 cards
-        for (int i = 1; i < 19; i++){
+        //18 move 1 cards
+        AddMovementCards(18, 1);
+        /*for (int i = 1; i < 19; i++){
             int prio = (int)Math.round(100*Math.random());
             cardDeck.add(new MovementCard(1, prio));
-        }
+        }*/
 
         //Move 2 cards
-        for (int i = 1; i < 13; i++){
+        AddMovementCards(12, 2);
+        /*for (int i = 1; i < 13; i++){
             int prio = (int)Math.round(100*Math.random());
             cardDeck.add(new MovementCard(2, prio));
-        }
+        }*/
 
         //Move 3 cards
-        for (int i = 1; i < 7; i++){
+        AddMovementCards(6, 3);
+        /*for (int i = 1; i < 7; i++){
             int prio = (int)Math.round(100*Math.random());
             cardDeck.add(new MovementCard(3, prio));
-        }
+        }*/
 
         //Move 1 back cards
-        for (int i = 1; i < 7; i++){
+        AddMovementCards(6, -1);
+        /*for (int i = 1; i < 7; i++){
             int prio = (int)Math.round(100*Math.random());
             cardDeck.add(new MovementCard(-1, prio));
-        }
+        }*/
 
         //Turn right cards
-        for (int i = 1; i < 19; i++){
+        AddTurnCards(18, true, false);
+        /*for (int i = 1; i < 19; i++){
             int prio = (int)Math.round(100*Math.random());
             cardDeck.add(new TurningCard(true, false, prio));
-        }
+        }*/
 
         //Turn left cards
-        for (int i = 1; i < 19; i++){
+        AddTurnCards(18, false, false);
+        /*for (int i = 1; i < 19; i++){
             int prio = (int)Math.round(100*Math.random());
             cardDeck.add(new TurningCard(false, false, prio));
-        }
+        }*/
 
         //Uturn cards
-        for (int i = 1; i < 7; i++){
+        AddTurnCards(6, true, true);
+        /*for (int i = 1; i < 7; i++){
             int prio = (int)Math.round(100*Math.random());
             cardDeck.add(new TurningCard(true, true, prio));
+        }*/
+    }
+
+    /**
+     * Adds amountOfCards amounts of movement cards with distance moveDistance
+     */
+    public void AddMovementCards(int amountOfCards, int moveDistance) {
+        for (int i = 0; i < amountOfCards; i++){
+            int prio = (int)Math.round(100*Math.random());
+            cardDeck.add(new MovementCard(moveDistance, prio));
+        }
+    }
+
+    /**
+     * Adds amountOfCards amounts of turn cards
+     */
+    public void AddTurnCards(int amountOfCards, boolean turnRight, boolean uTurn) {
+        for (int i = 0; i < amountOfCards; i++){
+            int prio = (int)Math.round(100*Math.random());
+            cardDeck.add(new TurningCard(turnRight, uTurn, prio));
         }
     }
 
@@ -61,6 +89,6 @@ public class Deck {
      * shuffle the deck
      */
     private void shuffle(){
-
+        Collections.shuffle(cardDeck);
     }
 }
