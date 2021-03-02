8e54482cde6047676f1f4dc37fb4faf9ffef1309
Anders Refvik Torsvik <andersrt@hotmail.no>
2021-03-02 16:13:01 +0100

    Lagt til klasser for multiplayer
    Lagt til en klasse for client, en for server og en for packet (det som skal sendes).
    
    Når brukeren åpner programmet bør den få mulighet mellom å "Host Game" eller "Join Game". Utifra dette vil brukeren enten være server eller klient.

---
+++ src/main/java/Multiplayer/PacketMessage.java
@@ -0,0 +1,7 @@
+package Multiplayer;
+
+import java.util.ArrayList;
+
+public class PacketMessage {
+    public ArrayList<String> message;
+}
---
+++ src/main/java/Multiplayer/ServerProgram.java
@@ -0,0 +1,57 @@
+package Multiplayer;
+
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Server;
+import com.esotericsoftware.kryonet.Listener;
+
+import java.util.ArrayList;
+
+public class ServerProgram extends Listener{
+    static Server server;
+    static int udpPort = 27960, tcpPort = 27960;
+    static boolean messageReceived = false;
+
+
+    public static void main(String[] args) throws Exception{
+        server = new Server();
+        server.getKryo().register(PacketMessage.class);
+        server.getKryo().register(java.util.ArrayList.class);
+        server.getKryo().register(String.class);
+        server.bind(tcpPort,udpPort);
+        server.start();
+        server.addListener(new ServerProgram());
+
+        while (!messageReceived){
+            //Thread.sleep(1000);
+        }
+    }
+
+    public void connected(Connection c){
+        System.out.println("Received a connection from " + c.getRemoteAddressTCP().getHostName());
+        PacketMessage packetMessage = new PacketMessage();
+        packetMessage.message = messageToSend();
+        c.sendTCP(packetMessage);
+    }
+
+    public void received(Connection c, Object p){
+        if(p instanceof PacketMessage){
+            PacketMessage packet = (PacketMessage) p;
+            System.out.println("Received a message from the client: " + packet.message);
+
+            messageReceived = true;
+        }
+    }
+
+    public void disconnected(Connection c){
+        System.out.println("A client disconnected");
+    }
+
+    public ArrayList<String> messageToSend() {
+        ArrayList<String> listToSend = new ArrayList<>();
+        listToSend.add("H");
+        listToSend.add("E");
+        listToSend.add("Y");
+        return listToSend;
+    }
+}
+
