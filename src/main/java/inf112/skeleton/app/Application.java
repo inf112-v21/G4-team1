f93f4afad985f56059f2626f60c4e75a790b37e5
AsgeirAlbretsen <bjellanda@gmail.com>
2021-03-05 13:06:50 +0100

    - Fiksa bug der ein robot forsvant dersom ein skubba ein anna robot mot ein vegg - Fiksa sonn at alle stega til robotten blir vist - La til printing når robotten flytter seg - Fiksa bug der uturn ikkje gjorde ein uturn - Fiksa bug der applikasjonen ikkje lukka seg skikkeleg når ein kryssa den ut - Fiksa bug der spelera som vant (plukka opp alle flagga) ikkje blei fjerna fra alle klientane sine skjerma

--- src/main/java/inf112/skeleton/app/Application.java
+++ src/main/java/inf112/skeleton/app/Application.java
@@ -148,25 +148,25 @@ public class Application extends InputAdapter implements ApplicationListener {
             //game.getPlayers().get(0).setPosition(game.getPlayers().get(0).getX(), game.getPlayers().get(0).getY() + 1);
             game.getPlayers().get(0).addCardToHand(new MovementCard(1, 0));
             game.getPlayers().get(0).setDirection("N");
-            game.getPlayers().get(0).moveBasedOnNextCard();
+            game.getPlayers().get(0).moveBasedOnNextCard(false);
             return true;
         }
         else if(keycode == Input.Keys.DOWN){
             game.getPlayers().get(0).addCardToHand(new MovementCard(1, 0));
             game.getPlayers().get(0).setDirection("S");
-            game.getPlayers().get(0).moveBasedOnNextCard();
+            game.getPlayers().get(0).moveBasedOnNextCard(false);
             return true;
         }
         else if(keycode == Input.Keys.LEFT){
             game.getPlayers().get(0).addCardToHand(new MovementCard(1, 0));
             game.getPlayers().get(0).setDirection("W");
-            game.getPlayers().get(0).moveBasedOnNextCard();
+            game.getPlayers().get(0).moveBasedOnNextCard(false);
             return true;
         }
         else if(keycode == Input.Keys.RIGHT){
             game.getPlayers().get(0).addCardToHand(new MovementCard(1, 0));
             game.getPlayers().get(0).setDirection("E");
-            game.getPlayers().get(0).moveBasedOnNextCard();
+            game.getPlayers().get(0).moveBasedOnNextCard(false);
             return true;
         }
 
